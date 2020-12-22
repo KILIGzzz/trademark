433f83aa75024c02427711f3e96bf31dda86b4e7
Enzo <enzopowershare@163.com>
2020-12-22 20:35:59 +0800

    整合2.31

--- src/main/java/com/cloudwise/trademark/controller/LoginController.java
+++ src/main/java/com/cloudwise/trademark/controller/LoginController.java
@@ -39,6 +39,10 @@ public class LoginController {
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         try {
             subject.login(token);
+            //在右上角显示用户名和照片
+            User user = (User) SecurityUtils.getSubject().getPrincipal();
+            request.getSession().setAttribute("username", user.getUserName());
+            request.getSession().setAttribute("userId", user.getUserId());
         } catch (UnknownAccountException e) {
             modelAndView.addObject("message", "account not found");
             modelAndView.setViewName("login");
@@ -46,11 +50,6 @@ public class LoginController {
             modelAndView.addObject("message", "incorrect password");
             modelAndView.setViewName("login");
         }
-        //在右上角显示用户名和照片
-        User user = (User) SecurityUtils.getSubject().getPrincipal();
-        //modelAndView.addObject("user", user);
-        request.getSession().setAttribute("username", user.getUserName());
-        request.getSession().setAttribute("userId", user.getUserId());
         // 在左侧显示树状的菜单导航。根据登录的用户名，查询该用户的所有菜单。
         List<LayUiTree> layUiTreeList = menuService.queryAllTreeByLoginName(username);
         modelAndView.addObject("layUiTreeList", layUiTreeList);
--- src/main/java/com/cloudwise/trademark/controller/UserController.java
+++ src/main/java/com/cloudwise/trademark/controller/UserController.java
@@ -4,11 +4,13 @@ import com.cloudwise.trademark.entity.PageBean;
 import com.cloudwise.trademark.entity.ReturnBean;
 import com.cloudwise.trademark.entity.User;
 import com.cloudwise.trademark.service.UserService;
+import com.cloudwise.trademark.shiro.ShiroUtil;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * 用户信息表(TblUser)表控制层
@@ -26,46 +28,46 @@ public class UserController extends BaseController {
     private UserService userService;
 
     /**
+     * @param pageBean:
+     * @param user:
+     * @return ReturnBean
      * @create by: IvanZ
      * @description : 查询所有，并分页，并按条件查询
      * @create time: 2020/12/17 10:09
-     * @param pageBean: 
- * @param user:
-     * @return ReturnBean
      */
     @GetMapping("findAll")
-    public ReturnBean findAll(PageBean pageBean, User user){
-        List<User> users = userService.queryAllByLimit(getOffset(pageBean), pageBean.getLimit(),user);
+    public ReturnBean findAll(PageBean pageBean, User user) {
+        List<User> users = userService.queryAllByLimit(getOffset(pageBean), pageBean.getLimit(), user);
         long count = userService.getCount(user);
-        ReturnBean returnBean = returnSuccess(users,count);
+        ReturnBean returnBean = returnSuccess(users, count);
         return returnBean;
     }
 
     /**
+     * @param userIds:
+     * @return com.cloudwise.trademark.entity.ReturnBean
      * @create by: IvanZ
      * @description : 根据选中的id删除数据
      * @create time: 2020/12/17 14:34
-     * @param userIds: 
-     * @return com.cloudwise.trademark.entity.ReturnBean
      */
     @PutMapping("delSelected")
-    public ReturnBean delSelected(String userIds){
+    public ReturnBean delSelected(String userIds) {
         boolean delete = userService.delSelected(userIds);
-        if(delete){
+        if (delete) {
             return returnSuccess(null);
         }
         return returnFail(null);
     }
-    
+
     /**
+     * @param userId:
+     * @return com.cloudwise.trademark.entity.ReturnBean
      * @create by: IvanZ
      * @description : 删除选中id的数据
      * @create time: 2020/12/17 14:38
-     * @param userId: 
-     * @return com.cloudwise.trademark.entity.ReturnBean
      */
     @PutMapping("deleteById")
-    public ReturnBean deleteById(Integer userId){
+    public ReturnBean deleteById(Integer userId) {
         boolean b = userService.deleteById(userId);
         if (b) {
             return returnSuccess(null);
@@ -74,25 +76,35 @@ public class UserController extends BaseController {
     }
 
     /**
+     * @param :
+     * @return com.cloudwise.trademark.entity.ReturnBean
      * @create by: IvanZ
      * @description : 添加用户
      * @create time: 2020/12/17 20:10
-     * @param : 
-     * @return com.cloudwise.trademark.entity.ReturnBean
      */
     @PostMapping("insertUser")
-    public ReturnBean insertUser(User user,String role){
-        //根据登录名查询用户
+    public ReturnBean insertUser(User user, String role) {
+        //根据登录名查询用户(用户名不能重复)
         User userTemp = userService.queryByLoginName(user.getLoginName());
         //如果用户不为空，返回错误
-        if (userTemp != null){
+        if (userTemp != null) {
             return returnFail(null);
         }
-        String[] roles= role.split(",");
+        String[] roles = role.split(",");
+        //加密之前的密码
+        String password = user.getPassword();
+        //随机一个盐值,传入数据库
+        String salt = String.valueOf(UUID.randomUUID());
+        user.setSalt(salt);
+        //shiro加密
+        ShiroUtil shiroUtil = new ShiroUtil();
+        //生成密文传入数据库
+        String cipher = shiroUtil.encryptionBySalt(salt, password);
+        user.setPassword(cipher);
         //往user表中添加数据
         User insert = userService.insert(user);
         //如果insert不为空，说明添加成功
-        if (insert!=null){
+        if (insert != null) {
             //根据返回的用户id往tbl_user_role中添加数据
             userService.insertUserRole(insert.getUserId(), roles);
             //返回成功
@@ -102,66 +114,76 @@ public class UserController extends BaseController {
     }
 
     /**
+     * @param :
+     * @return java.util.List<java.util.Map < java.lang.String, java.lang.Object>>
      * @create by: IvanZ
      * @description : 查询所有部门
      * @create time: 2020/12/17 19:54
-     * @param :
-     * @return java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
      */
     @GetMapping("findAllDept")
-    public List<Map<String, Object>> findAllDept(){
+    public List<Map<String, Object>> findAllDept() {
         List<Map<String, Object>> allDept = userService.findAllDept();
         return allDept;
     }
 
     /**
+     * @param :
+     * @return java.util.List<java.util.Map < java.lang.String, java.lang.Object>>
      * @create by: IvanZ
      * @description : 查询所有角色
      * @create time: 2020/12/17 19:53
-     * @param :
-     * @return java.util.List<java.util.Map<java.lang.String,java.lang.Object>>
      */
     @GetMapping("findAllRole")
-    public List<Map<String, Object>> findAllRole(){
+    public List<Map<String, Object>> findAllRole() {
         List<Map<String, Object>> allRole = userService.findAllRole();
         return allRole;
     }
 
     /**
+     * @param user:
+     * @return com.cloudwise.trademark.entity.ReturnBean
      * @create by: IvanZ
      * @description : 修改用户信息
      * @create time: 2020/1332/18 15:48
-     * @param user: 
-     * @return com.cloudwise.trademark.entity.ReturnBean
      */
     @PutMapping("updateUser")
-    public ReturnBean updateUser(User user,String role){
+    public ReturnBean updateUser(User user, String role) {
         User update = userService.update(user);
-        if (update != null){
-            userService.updateUserRole(user.getUserId(),role);
+        if (update != null) {
+            userService.updateUserRole(user.getUserId(), role);
             return returnSuccess(null);
         }
         return returnFail(null);
     }
 
     /**
+     * @param user:
+     * @return com.cloudwise.trademark.entity.ReturnBean
      * @create by: IvanZ
      * @description : 修改密码
      * @create time: 2020/12/18 18:42
-     * @param user: 
-     * @return com.cloudwise.trademark.entity.ReturnBean
      */
     @PutMapping("updatePassword")
-    public ReturnBean updatePassword(User user){
+    public ReturnBean updatePassword(User user) {
+        //加密之前的密码
+        String password = user.getPassword();
+        //随机一个盐值,传入数据库
+        String salt = String.valueOf(UUID.randomUUID());
+        user.setSalt(salt);
+        //shiro加密
+        ShiroUtil shiroUtil = new ShiroUtil();
+        //生成密文传入数据库
+        String cipher = shiroUtil.encryptionBySalt(salt, password);
+        user.setPassword(cipher);
         User update = userService.update(user);
-        if (update != null){
+        if (update != null) {
             return returnSuccess(null);
         }
         return returnFail(null);
     }
 
     @GetMapping("findSelectRole")
-    public List<Integer> findSelectRole(int userId){
+    public List<Integer> findSelectRole(int userId) {
         return userService.findSelectRole(userId);
     }
 }

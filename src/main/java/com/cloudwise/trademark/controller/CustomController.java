3745c203116c8464dff2e4dbbbf8e2341cdc8f5c
Enzo <enzopowershare@163.com>
2020-12-28 23:43:51 +0800

    版本1.81

--- src/main/java/com/cloudwise/trademark/controller/CustomController.java
+++ src/main/java/com/cloudwise/trademark/controller/CustomController.java
@@ -97,7 +97,14 @@ public class CustomController extends BaseController {
             return returnFail(null);
         }
     }
-
+    /**
+     * 导入Excel数据
+     * 
+     * @param file excel文件
+     * @return ReturnBean
+     * @createBy Enzo
+     * @createTime 2020/12/28 23:43
+     */
     @PostMapping("excelInsert")
     public ReturnBean excelInsert(MultipartFile file) {
         try {

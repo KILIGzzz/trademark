a65083908076e3e4697f8f6923e4fc3d97e4d5db
KILIGzzz <email>
2020-12-29 22:42:00 +0800

    统计Controller

---
+++ src/main/java/com/cloudwise/trademark/controller/StatisticalController.java
@@ -0,0 +1,36 @@
+package com.cloudwise.trademark.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Enzo
+ * @version 1.0
+ * @date Created at 2020/12/29 15:47
+ * @description 统计管理控制器
+ * @modifiedBy
+ */
+@Controller
+@RequestMapping("statistical")
+public class StatisticalController extends BaseController {
+    @GetMapping("toBusinessEchart")
+    public String toBusinessEchart() {
+        return "businessEchart";
+    }
+
+    @GetMapping("toProgressEchart")
+    public String toProgressEchart() {
+        return "progressEchart";
+    }
+
+    @GetMapping("toVisitEchart")
+    public String toVisitEchart() {
+        return "visitEchart";
+    }
+
+    @GetMapping("toAttendance")
+    public String toAttendance() {
+        return "attendance";
+    }
+}
